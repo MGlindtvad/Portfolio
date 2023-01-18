175d24c70c0996017418d55ffeb75851ac383d9a
Michala <mvg92@outlook.com>
2023-01-18 20:38:50 +0100

    String clean up in the variable cast

--- Doctor Who analysis.R
+++ Doctor Who analysis.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(dplyr)
 library(readr)
 library(draw)
+library(rlang)
+library(stringr)
 
 ##Loading the data from Kaggel
 dwguide <- read_csv("dwguide.csv")
@@ -15,16 +17,36 @@ str(allscript, give.att = FALSE)
 str(allepisodes, give.att = FALSE)
 str(imdb_details, give.att = FALSE)
 
+
+
 #left join dwguide with allepisodes
 dw_episode <- dwguide %>% select(!(cast:summary)) %>%
-          left_join(allepisodes, by = "title", suffix = c(".guide",".episode")) 
-          
+  left_join(allepisodes, by = "title", suffix = c(".guide",".episode"))%>% 
+  arrange(episodenbr)
+
 ##Number of episode per doctor
 dw_episode %>% group_by(doctorid) %>%
-               count()
+  count()
+
 ##There is 716 NA's which means we must find a way to determin the doctor of an episode in an other way.
 
+#Finding The doctors in dwguide
+dwguide$cast
+
+#The way to find the Doctors are in the variable cast and that variable is a long string containing 
+Doctor <- data.frame(episodenbr = 1:nrow(dw_episode),DoctorText = rep("No Doctor", nrow(dw_episode)))
+
+for(i in dw_episode$episodenbr){Doctor$DoctorText[i] <- unlist(strsplit(dwguide$cast[i], split = ","))[2]}
+
+Doctor$DoctorText <- str_replace_all(str_replace_all(Doctor$DoctorText, "[^[:alnum:]]", ""), "name","")
+
+##Checking to see that only the Doctors was 
+unique(Doctor$DoctorText)
+
+##One is just call Dr. Who and we want to know how many episodes has this as a cast
+Doctor %>% filter(DoctorText == "DrWho") 
+
 ##Which doctor has the maximum, minimum and mean of episodes
-
 ##To be continued
-
+  
+  
